2446230439e62e6c16395e3e4b5af3d6916e2964
Jen <jenniferanndcb@gmail.com>
2018-05-12 22:55:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // write your code below!
-function happyHolidays()
-  return "Happy holidays!"; 
+function happyHolidays() {
+  return "Happy holidays!"
+  }; 
   
-function happyHolidaysTo (name)
-  return `Happy holidays, ${name}!`;
+function happyHolidaysTo (name){
+  return `Happy holidays, ${name}!`
+  
+};
   
